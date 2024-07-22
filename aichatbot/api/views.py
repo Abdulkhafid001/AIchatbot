bb32c64f02235d74c3de9760b25c51168b17a530
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-07-22 07:37:28 +0400

    configure get

--- aichatbot/api/views.py
+++ aichatbot/api/views.py
@@ -4,10 +4,13 @@ from rest_framework.decorators import api_view
 from .serializers import ItemSerializer
 from myrestapi.models import Item
 # from  import ItemSerializer
+
+
 @api_view(['GET'])
 def get_data(request):
     # get all database data
     items = Item.objects.all()
+    print(items)
     # serialize to json
     serializer = ItemSerializer(items)
     return Response(serializer.data)
