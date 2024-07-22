8df65ade2b77d8ef63a69ecbe42d2f1198ae2898
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-07-22 09:20:28 +0400

    testing drf docs api

--- aichatbot/api/serializers.py
+++ aichatbot/api/serializers.py
@@ -1,8 +1,22 @@
+# Serializers take querysets and model instances and convert them into native Python data types.
+# We can then take those data types and render them into JSON.
 from rest_framework import serializers
-from myrestapi.models import Item
+
+from django.contrib.auth.models import Group, User
 
 
-class ItemSerializer(serializers.ModelSerializer):
+# user serializer
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Item
-        fields = '__all__'
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+# group user
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
