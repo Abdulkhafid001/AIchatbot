3bcd37e02e5de2fc43da2b1f0fdf5044e65acaaa
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-07-22 06:19:06 +0400

    adding api endpoint serializers

--- aichatbot/api/views.py
+++ aichatbot/api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.response import Response
 # used to write the api views
 from rest_framework.decorators import api_view
-
+from .serializers import ItemSerializer
+from myrestapi.models import Item
+# from  import ItemSerializer
 @api_view(['GET'])
 def get_data(request):
-    person = {"name": "Abdulkhafid"}
-    return Response(person)
+    # get all database data
+    items = Item.objects.all()
+    # serialize to json
+    serializer = ItemSerializer()
+    return Response()
