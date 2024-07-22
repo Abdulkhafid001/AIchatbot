e278216d279066e5e00dc0472d7bd8790fa37584
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-07-22 13:49:17 +0400

    added more DRF functionality

--- aichatbot/myrestapi/urls.py
+++ aichatbot/myrestapi/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from myrestapi import views
 urlpatterns = [
     path("snippets/", views.snippet_list, name=""),
     path("snippets/<int:pk>/", views.snippet_detail, name="")
 ]
+
+# append a set of format_suffix_patterns in addition to the existing URLs.
+urlpatterns = format_suffix_patterns(urlpatterns)
