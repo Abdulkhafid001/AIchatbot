dcd14bd180c6462eeb69af953d93c8e0dab3d50f
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-07-23 13:34:49 +0400

    add DRF pagination

--- aichatbot/aichatbot/settings.py
+++ aichatbot/aichatbot/settings.py
@@ -115,3 +115,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# change the default list style of the Snippet API to use pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
