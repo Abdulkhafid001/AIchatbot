89b9b4391050bc0abf00b4bf0d39db0150cb5a17
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-07-23 09:58:21 +0400

    add permissions to the views using DRf permissions

--- aichatbot/myrestapi/views.py
+++ aichatbot/myrestapi/views.py
@@ -4,16 +4,17 @@ from rest_framework import generics
 from django.contrib.auth.models import User
 from myrestapi.serializers import UserSerializer
 
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
     # associating Snippets with Users
-    # The way we deal with that is by overriding a .perform_create() method on our snippet views, 
-    # that allows us to modify how the instance save is managed, 
-    # and handle any information that is implicit in the incoming request or requested URL.
+    # this method is always called when a serializer is created 
+    # so we pass a snippet owner to the serializer everytime a new snippet is created
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
