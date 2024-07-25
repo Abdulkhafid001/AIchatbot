286a660d204334d31c4549b94e37a9bc245f3185
Abdulkhafid Kabiru <kabiruabdulkhafid@gmail.com>
2024-07-25 11:08:16 +0400

    adding the models for the API

--- aichatbot/blog/models.py
+++ aichatbot/blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class Blog(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=200)
+    # slug = models.SlugField(max_length=200, unique=True, blank=True)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    # tags = models.ManyToManyField('Tag', blank=True)
+    featured_image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-published']
+        indexes = [
+            models.Index(fields=['-published']),
+            models.Index(fields=['slug']),
+        ]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
